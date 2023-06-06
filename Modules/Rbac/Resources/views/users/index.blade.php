7a98f5fce25eb1f9f6091de552c8ca4e42e80252
Islam2718 <mislamhn@gmail.com>
2023-06-06 14:16:15 +0600

    admin assets included

--- Modules/Rbac/Resources/views/users/index.blade.php
+++ Modules/Rbac/Resources/views/users/index.blade.php
@@ -1,5 +1,46 @@
 @include('admin-panel.header')
 @include('admin-panel.sidebar')
-<h1>hello users</h1>
+<!-- users content  -->
+<div class="container-xxl flex-grow-1 container-p-y">
+  <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Rbac /</span> Users</h4>
 
+  <!-- Basic Bootstrap Table -->
+  <div class="card">
+    <h5 class="card-header">Table Basic</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Sl</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>1</strong></td>
+            <td>
+              <img src="../admin-assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" style="width:22px;">
+              &nbsp; 
+              Abdur Rahman              
+            </td>
+            <td>demo@mail.com</td>
+            <td><span class="badge bg-label-primary me-1">Admin</span></td>
+            <td><span class="badge bg-label-success me-1">Active</span></td>
+            <td>
+              <button class="btn btn-primary">Edit</button>
+              <button class="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <!--/ Basic Bootstrap Table -->
+
+</div>
+<!-- users content end  -->
 @include('admin-panel.footer')
--- resources/views/admin-panel/footer.blade.php
+++ resources/views/admin-panel/footer.blade.php
@@ -1,1 +1,57 @@
-<h1>#footer</h1>
+
+            <!-- Footer -->
+            <footer class="content-footer footer bg-footer-theme">
+              <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                <div class="mb-2 mb-md-0"><!--more links--></div>
+                <div>
+                  © <script> document.write(new Date().getFullYear());</script> , made with ❤️ by
+                  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">Grameen Communications</a>                
+                </div>
+              </div>
+            </footer>
+            <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+      </div>
+
+      <!-- Overlay -->
+      <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <!-- <div class="buy-now">
+      <a
+        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+        target="_blank"
+        class="btn btn-danger btn-buy-now"
+        >Developeb By &mdash; GC</a
+      >
+    </div> -->
+
+    <!-- Core JS -->
+    <!-- build:js admin-assets/vendor/js/core.js -->
+    <script src="../admin-assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../admin-assets/vendor/libs/popper/popper.js"></script>
+    <script src="../admin-assets/vendor/js/bootstrap.js"></script>
+    <script src="../admin-assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../admin-assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="../admin-assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="../admin-assets/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="../admin-assets/js/dashboards-analytics.js"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
