488fa80298e0caffff60379025f8f8dc72ccbca3
arafatkazi19 <arafatkazi47@gmail.com>
2023-06-07 14:44:03 +0600

    people schema modified

--- Modules/Profile/Database/Migrations/2023_06_06_095514_create_people_table.php
+++ Modules/Profile/Database/Migrations/2023_06_06_095514_create_people_table.php
@@ -16,32 +16,32 @@ return new class extends Migration
         Schema::create('people', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->integer('signup_category_id');
-            $table->integer('signup_sub_category_id');
+            $table->integer('signup_category_id')->nullable();
+            $table->integer('signup_sub_category_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('sur_name');
             $table->string('email');
-            $table->string('identity');
+            $table->string('identity')->nullable();
             $table->string('gender');
-            $table->string('organization_name');
-            $table->bigInteger('organization_id');
+            $table->string('organization_name')->nullable();
+            $table->bigInteger('organization_id')->nullable();
 
             $table->date('dob');
-            $table->string('profile_image');
-            $table->string('cover_image');
-            $table->string('online_profile');
+            $table->string('profile_image')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->string('online_profile')->nullable();
 
-            $table->text('interested_in');
-            $table->text('about_myself');
+            $table->text('interested_in')->nullable();
+            $table->text('about_myself')->nullable();
 
-            $table->string('facebook');
-            $table->string('twitter');
-            $table->string('linkedin');
-            $table->string('google');
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('google')->nullable();
 
-            $table->tinyInteger('is_organization_root_user');
-            $table->integer('status');
+            $table->tinyInteger('is_organization_root_user')->default(0);
+            $table->integer('status')->default(0);
             $table->timestamps(); //created-at, updated-at 
 
              $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
